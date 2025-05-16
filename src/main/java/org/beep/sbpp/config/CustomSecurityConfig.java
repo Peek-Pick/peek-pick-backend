16d43bbf80fc832a9e88746e2e116d43f80b5d74
KimSoYeonnn <10sosokim10@gmail.com>
2025-05-16 09:40:41 +0900

    init

---
+++ src/main/java/org/beep/sbpp/config/CustomSecurityConfig.java
@@ -0,0 +1,56 @@
+package org.beep.sbpp.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@Configuration
+public class CustomSecurityConfig {
+    
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+
+        log.info("----------configure---------");
+        
+        //로그인 화면 필요 없음 (Headless)
+        http.formLogin(config -> {
+            config.disable();
+        });
+
+        //세션 생성 필요 없음
+        http.sessionManagement(config -> {
+            config.sessionCreationPolicy(SessionCreationPolicy.NEVER);
+        });
+
+        return http.build();
+    }
+    
+    //CORS 설정
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        corsConfiguration.setAllowedOriginPatterns(List.of("*")); // 어디서든 허락
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"));
+        corsConfiguration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+        corsConfiguration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return source;
+    }
+
+}
+
