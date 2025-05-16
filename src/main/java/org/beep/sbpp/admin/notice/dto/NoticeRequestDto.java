1fedbd74822b23e84c75b4c06a585c24451abdf0
Donghun Kang <lmdnhn00@gmail.com>
2025-05-16 17:44:28 +0900

    [🌱feat] 공지사항 백엔드 기능 구현
    - 이미지 업로드 기능은 아직 X

---
+++ src/main/java/org/beep/sbpp/admin/notice/dto/NoticeRequestDto.java
@@ -0,0 +1,25 @@
+package org.beep.sbpp.admin.notice.dto;
+
+import java.util.List;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class NoticeRequestDto {
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String content;
+
+    private List<String> imgUrls;
+}
