ee18d14562ea5cfb104c13f54312a5fd2126865b
KimSoYeonnn <10sosokim10@gmail.com>
2025-05-21 17:24:42 +0900

    [📦 refactor] tag 테이블에 category 컬럼 추가
    -tag 테이블에 category 컬럼 추가
    -seed data test 코드로 추가

--- src/main/java/org/beep/sbpp/reviews/repository/ReviewImgRepository.java
+++ src/main/java/org/beep/sbpp/reviews/repository/ReviewImgRepository.java
@@ -9,8 +9,10 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface ReviewImgRepository extends JpaRepository<ReviewImgEntity, Long> {
+    List<ReviewImgEntity> findAllByReviewEntity_ReviewId(Long reviewId);
+
     @Query("""
-                select new org.beep.sbpp.reviews.dto.ReviewImgDTO(ri.reviewImgId, ri.reviewEntity.reviewId, ri.imgUrl)
+                select new org.beep.sbpp.reviews.dto.ReviewImgDTO(ri.reviewEntity.reviewId, ri.imgUrl)
                 from ReviewImgEntity ri
                 where ri.reviewEntity.reviewId = :reviewId
                 order by ri.reviewImgId asc
--- src/test/java/org/beep/sbpp/users/UserServiceTest.java
+++ src/test/java/org/beep/sbpp/users/UserServiceTest.java
@@ -19,6 +19,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.annotation.Commit;
+import org.springframework.test.context.TestPropertySource;
 
 import java.time.LocalDate;
 import java.util.Collections;
@@ -31,6 +32,11 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @Transactional
 @Slf4j
+@TestPropertySource(properties = {
+        "GOOGLE_CLIENT_ID=test-client-id",
+        "GOOGLE_CLIENT_SECRET=test-secret",
+        "GOOGLE_REDIRECT_URI=http://localhost:8080/oauth2/callback"
+})
 public class UserServiceTest {
 
     @Autowired
