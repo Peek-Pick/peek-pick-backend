adfd5a3e595e0e64d18082b1bc31f635ccbd717b
KimSoYeonnn <10sosokim10@gmail.com>
2025-05-24 22:11:12 +0900

    [🌱 feat] 회원가입 API 구현
    - 회원가입 시 사용자 정보, 프로필, 태그 정보를 한 번에 처리하도록 API 구현

--- src/main/java/org/beep/sbpp/users/controller/UserController.java
+++ src/main/java/org/beep/sbpp/users/controller/UserController.java
@@ -3,10 +3,12 @@ package org.beep.sbpp.users.controller;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.beep.sbpp.tags.dto.TagSelectionDTO;
 import org.beep.sbpp.common.ActionResultDTO;
 import org.beep.sbpp.users.dto.UserDTO;
 import org.beep.sbpp.users.dto.UserProfileDTO;
+import org.beep.sbpp.users.dto.UserSignupRequestDTO;
 import org.beep.sbpp.users.repository.UserRepository;
 import org.beep.sbpp.users.service.UserService;
 import org.springframework.http.HttpStatus;
@@ -18,36 +20,43 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
+@Slf4j
 public class UserController {
 
     private final UserService userService;
-    private final UserRepository userRepository;
 
     @PostMapping("/signup")
-    public ResponseEntity<ActionResultDTO<Long>> signup(UserDTO dto) {
-
-        Long userId = userService.signup(dto);
-
+    public ResponseEntity<ActionResultDTO<Long>> fullsignup(@RequestBody UserSignupRequestDTO dto) {
+        log.info("Controller tagIdList: {}", dto);
+        Long userId = userService.fullSignup(dto);
         return ResponseEntity.ok(ActionResultDTO.success(userId));
-
     }
 
-    @PostMapping("/signup/profile")
-    public ResponseEntity<ActionResultDTO<Long>> profileRegister(
-            @RequestParam("userId") Long userId,
-            @RequestBody UserProfileDTO dto) {
-
-        Long resultId = userService.profileRegister(userId, dto);
-        return ResponseEntity.ok(ActionResultDTO.success(resultId));
-    }
-
-    @PostMapping("/signup/tags")
-    public ResponseEntity<ActionResultDTO<Long>> userTagRegister(@RequestBody TagSelectionDTO dto){
-
-        Long resultUserTag = userService.userTagRegister(dto.getUserId(), dto.getTagIdList());
-
-        return ResponseEntity.ok(ActionResultDTO.success(resultUserTag));
-    }
+//    @PostMapping("/signup")
+//    public ResponseEntity<ActionResultDTO<Long>> signup(UserDTO dto) {
+//
+//        Long userId = userService.signup(dto);
+//
+//        return ResponseEntity.ok(ActionResultDTO.success(userId));
+//
+//    }
+//
+//    @PostMapping("/signup/profile")
+//    public ResponseEntity<ActionResultDTO<Long>> profileRegister(
+//            @RequestParam("userId") Long userId,
+//            @RequestBody UserProfileDTO dto) {
+//
+//        Long resultId = userService.profileRegister(userId, dto);
+//        return ResponseEntity.ok(ActionResultDTO.success(resultId));
+//    }
+//
+//    @PostMapping("/signup/tags")
+//    public ResponseEntity<ActionResultDTO<Long>> userTagRegister(@RequestBody TagSelectionDTO dto){
+//
+//        Long resultUserTag = userService.userTagRegister(dto.getUserId(), dto.getTagIdList());
+//
+//        return ResponseEntity.ok(ActionResultDTO.success(resultUserTag));
+//    }
 
 
     @PutMapping("/{userId}")
