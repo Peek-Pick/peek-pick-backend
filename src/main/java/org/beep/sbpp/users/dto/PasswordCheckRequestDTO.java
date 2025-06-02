9991d77dab4b3386f3b26af15313e56fad2fa1c9
KimSoYeonnn <10sosokim10@gmail.com>
2025-06-02 18:05:54 +0900

    [🌱 feat] 비밀번호 확인 기능

---
+++ src/main/java/org/beep/sbpp/users/dto/PasswordCheckRequestDTO.java
@@ -0,0 +1,14 @@
+package org.beep.sbpp.users.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PasswordCheckRequestDTO {
+    private String password;
+}
