83042d7a3862ceb619c80bb35f916abaae5b88f5
KimSoYeonnn <10sosokim10@gmail.com>
2025-06-13 17:22:27 +0900

    [🔍 style] 코드 포멧 수정
    - 사용하지 않는 import 삭제
    - 로그 수정

--- src/main/java/org/beep/sbpp/users/scheduler/UserStatusScheduler.java
+++ src/main/java/org/beep/sbpp/users/scheduler/UserStatusScheduler.java
@@ -2,6 +2,7 @@ package org.beep.sbpp.users.scheduler;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.beep.sbpp.users.entities.UserEntity;
 import org.beep.sbpp.users.enums.Status;
 import org.beep.sbpp.users.repository.UserRepository;
@@ -9,10 +10,12 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class UserStatusScheduler {
 
     private final UserRepository userRepository;
@@ -20,6 +23,7 @@ public class UserStatusScheduler {
     @Transactional
     @Scheduled(cron = "0 0 0 * * *") // 자정마다 (cron = "초 분 시 일 월 요일")
     public void unbanUsers() {
+        log.info("✅ 스케줄러 작동: " + LocalDateTime.now());
         List<UserEntity> expired = userRepository.findByStatusAndBanUntilBefore(Status.BANNED, LocalDate.now());
         for (UserEntity user : expired) {
             user.setStatus(Status.ACTIVE);
