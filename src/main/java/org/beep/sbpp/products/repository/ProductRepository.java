83ba063c0ccddbd3cba5415dffdd868445dd6040
BOOK-8FHO8G5AP5\admin <leedmswls@gmail.com>
2025-06-18 17:38:04 +0900

    [🌱 feat] countWishCountByUserId 위시 상품 개수 조회

--- src/main/java/org/beep/sbpp/products/repository/ProductRepository.java
+++ src/main/java/org/beep/sbpp/products/repository/ProductRepository.java
@@ -2,12 +2,14 @@ package org.beep.sbpp.products.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.beep.sbpp.products.entities.ProductEntity;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
-public interface ProductRepository
-        extends JpaRepository<ProductEntity, Long>, ProductRepositoryCustom {
+public interface ProductRepository extends JpaRepository<ProductEntity, Long>, ProductRepositoryCustom {
 
     Optional<ProductEntity> findByBarcode(String barcode);
 
+    @Query("SELECT COUNT(p) FROM ProductLikeEntity p WHERE p.userEntity.userId = :userId AND p.isDelete = false ")
+    Long countWishByUserId(Long userId);
 }
--- src/main/java/org/beep/sbpp/reviews/service/ReviewServiceImpl.java
+++ src/main/java/org/beep/sbpp/reviews/service/ReviewServiceImpl.java
@@ -36,8 +36,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-
-
 @Slf4j
 @Service
 @Transactional
