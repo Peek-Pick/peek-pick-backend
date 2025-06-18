7c36bccddd8b7b04c2b0a2211a3a62383e36e7cd
bkm00 <bkm0096@gmail.com>
2025-06-18 10:42:06 +0900

    [🌱 feat] getAuthUserEmail 추가

--- src/main/java/org/beep/sbpp/util/UserInfoUtil.java
+++ src/main/java/org/beep/sbpp/util/UserInfoUtil.java
@@ -24,8 +24,7 @@ public class UserInfoUtil {
             ..
         }
 
-        *** 이제 uid에 로그인한 사용자의 id가 들어있음
-        *** email을 id처럼 바로 파싱하고 싶으면 아래에 getAuthUserEmail 작성할 것. 사용법은 동일함    */
+        *** 이제 uid에 로그인한 사용자의 id가 들어있음                                  */
 
     public Long getAuthUserId(HttpServletRequest request) {
         // 1. 쿠키에서 accessToken 추출
@@ -49,5 +48,29 @@ public class UserInfoUtil {
         if (uidObj == null) throw new RuntimeException("Token에 uid 정보 없음");
 
         return Long.parseLong(uidObj.toString());
+    }
+
+    public String getAuthUserEmail(HttpServletRequest request) {
+        // 1. 쿠키에서 accessToken 추출
+        String accessToken = null;
+        if (request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                if ("accessToken".equals(cookie.getName())) {
+                    accessToken = cookie.getValue();
+                    break;
+                }
+            }
+        }
+
+        if (accessToken == null) {
+            throw new RuntimeException("accessToken 쿠키 없음");
+        }
+
+        // 2. JWT 파싱해서 uid 추출
+        Map<String, Object> claims = jwtUtil.validateToken(accessToken);
+        Object uemObj = claims.get("uem");
+        if (uemObj == null) throw new RuntimeException("Token에 uem 정보 없음");
+
+        return uemObj.toString();
     }
 }
